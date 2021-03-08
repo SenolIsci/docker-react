40239fa1fbf61be9186e6dba993373e9b9c8296f
SENOL ISCI <senol.isci@gmail.com>
2021-03-08 17:17:24 +0300

    deployment

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello there! This is an example of a prod-grade CI/CD workflow through integration: local(commit push to github) -> github (code base)-> travis (tests and deploys to)> AWS elastic beanstalk. </p>
       </header>
     </div>
   );
